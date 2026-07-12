262e4c65b5dccbf172579365a00114b611ac79cb
Yukino <f1704@yahoo.com>
2026-07-12 01:49:26 -0400

    fix: linux admin permission

--- lib/permission.py
+++ lib/permission.py
@@ -3,16 +3,27 @@ import sys
 import os
 
 def is_admin():
-    try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
-        return False
-    
+    if os.name == 'nt':
+        try:
+            return ctypes.windll.shell32.IsUserAnAdmin()
+        except Exception:
+            return False
+            
+    return True
+
 def run_admin():
-    executable = sys.executable
-    if executable.lower().endswith(("python.exe", "pythonw.exe")):
-        params = " ".join([f'"{arg}"' for arg in sys.argv])
-    else:
-        params = " ".join([f'"{arg}"' for arg in sys.argv[1:]])
-    res = ctypes.windll.shell32.ShellExecuteW(None, "runas", executable, params, None, 1)
-    return res > 32
+    if os.name == 'nt':
+        executable = sys.executable
+        if executable.lower().endswith(("python.exe", "pythonw.exe")):
+            params = " ".join([f'"{arg}"' for arg in sys.argv])
+        else:
+            params = " ".join([f'"{arg}"' for arg in sys.argv[1:]])
+            
+        try:
+            res = ctypes.windll.shell32.ShellExecuteW(None, "runas", executable, params, None, 1)
+            return res > 32
+        except Exception:
+            return False
+            
+    print("Admin not supported, or no need.")
+    return False
